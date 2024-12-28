1b0e15352409ea3b488ca73649ccc84b3e410af8
Jack Burkhardt <mail@jackburkhardt.com>
2024-12-28 11:56:48 -0600

    prettier nodes, finally "fixed" the way global variable types are read

--- OEIKnapper/GlobalVariable.cs
+++ OEIKnapper/GlobalVariable.cs
@@ -8,7 +8,7 @@ public class GlobalVariable : IBundleItem
     public Guid ID { get; set; }
     public string Tag { get; set; } // "b" prefix is bool, otherwise int...?
     [JsonProperty("VariableType")]
-    public GlobalVarDataType Type { get; set; } // todo: what is this? not a datatype it seems
+    public GlobalVarDataType Type { get; set; } // this is not a data type in the OEI schema, but I can't figure it out.
     public string InitialValue { get; set; }
     
     public static GlobalVariable TryParse(JToken json)
@@ -17,12 +17,14 @@ public class GlobalVariable : IBundleItem
         {
             throw new ArgumentException("Unable to parse GlobalVariable from: " + json.ToString(Formatting.None));
         }
+
+        GlobalVarDataType varType = json["Tag"].Value<string>().StartsWith('b') ? GlobalVarDataType.Boolean : GlobalVarDataType.Integer;
         
         return new GlobalVariable
         {
             ID = json["ID"].ToObject<Guid>(),
             Tag = json["Tag"].Value<string>(),
-            Type = OEIJsonUtils.ParseEnum(json["VariableType"], GlobalVarDataType.Boolean),
+            Type = varType,
             InitialValue = json["InitialValue"]?.Value<string>() ?? ""
         };
     }
